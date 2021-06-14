6aed6e83af6a92bf295161c3a526aa1d1da5c5e4
tuanbui0509 <tuanbui0509@gmail.com>
2021-06-14 10:25:36 +0700

    install react for project

---
+++ react-employee-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Button from 'react-bootstrap/Button'
+function App() {
+  return (
+    <div>
+      <Button variant='primary' >bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
